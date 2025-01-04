1ada2f84950585b9015072c88be19d9a8a28e109
ecthon <ecthon@gmail.com>
2025-01-04 13:24:35 -0400

    Atualizar descrição do aplicativo e melhorar a responsividade do formulário de sorteio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,20 +23,20 @@ export default function Home() {
 
   return (
     <div
-      className="w-full h-screen bg-center bg-repeat"
+      className="flex w-full max-sm:w-full bg-center bg-repeat-y"
       style={{ backgroundImage: "url('/bg.svg')" }}
     >
-      <div className="flex flex-col items-center justify-center w-full h-full bg-gradient-to-b from-black/90 to-black/50">
-        <div className="flex flex-col w-[392px]">
+      <div className="flex flex-col items-center justify-center max-sm:w-full w-full min-h-screen bg-gradient-to-b from-black/90 to-black/50 ms:px-5 pt-40">
           <form
-            className="flex w-[392px] flex-col gap-8 items-center"
+            className="flex w-[500px] max-sm:w-full flex-col gap-8 items-center max-sm:px-5"
             onSubmit={handleSubmit}
           >
-            <div className="flex gap-4">
-              <div className="flex flex-col items-center gap-2 justify-center bg-black/90 rounded-lg">
+            <h1 className="text-5xl font-bold leading-tight mb-5">Sorteador de números.</h1>
+            <div className="flex gap-4 ">
+              <div className="flex flex-col items-center gap-2 justify-center rounded-lg">
                 <p className="font-bold uppercase text-zinc-300">Número(s)</p>
                 <input
-                  className="flex pl-4 text-center font-bold ring-0 focus:ring-0 focus:outline-none text-4xl items-center  justify-center w-[120px] h-[56px] rounded-lg border-none bg-[#111012]"
+                  className="flex text-center font-bold ring-0 focus:ring-0 focus:outline-none text-4xl max-sm:text-3xl items-center  justify-center w-full h-[56px] rounded-lg border-none bg-[#111012]"
                   type="number"
                   name="quantity"
                   id="quantity"
@@ -44,20 +44,20 @@ export default function Home() {
                   defaultValue={1}
                 />
               </div>
-              <div className="flex flex-col items-center gap-2 justify-center bg-black/90 rounded-lg">
+              <div className="flex flex-col items-center gap-2 justify-center rounded-lg">
                 <p className="font-bold uppercase text-zinc-300">De</p>
                 <input
-                  className="flex pl-4 text-center font-bold ring-0 focus:ring-0 focus:outline-none text-4xl items-center justify-center w-[120px] h-[56px] rounded-lg border-none bg-[#111012]"
+                  className="flex text-center font-bold ring-0 focus:ring-0 focus:outline-none text-4xl max-sm:text-3xl items-center justify-center w-full h-[56px] rounded-lg border-none bg-[#111012]"
                   type="number"
                   name="from"
                   id="from"
                   defaultValue={0}
                 />
               </div>
-              <div className="flex flex-col items-center gap-2 justify-center bg-black/90 rounded-lg">
+              <div className="flex flex-col items-center gap-2 justify-center rounded-lg">
                 <p className="font-bold uppercase text-zinc-300">Até</p>
                 <input
-                  className="flex pl-4 text-center font-bold ring-0 focus:ring-0 focus:outline-none text-4xl items-center justify-center w-[120px] h-[56px] rounded-lg border-none bg-[#111012]"
+                  className="flex text-center font-bold ring-0 focus:ring-0 focus:outline-none text-4xl max-sm:text-3xl items-center justify-center w-full h-[56px] rounded-lg border-none bg-[#111012]"
                   type="number"
                   name="to"
                   id="to"
@@ -72,21 +72,20 @@ export default function Home() {
               Sortear
             </button>
           </form>
-          <div className="flex flex-col mt-10 gap-8 items-center">
+          <div className="flex flex-col w-[500px] max-sm:w-full my-10 gap-8 items-center max-sm:px-5">
             <h1>Resultado do sorteio</h1>
-            <p>{count}º Resultado</p>
+            <p className="text-zinc-300">{count}ª Rodada</p>
             <div className="flex w-full gap-4 flex-wrap">
               {results.map((result, index) => (
                 <p
                   key={index}
-                  className="flex items-center bg-[#C58DE7]/10 backdrop-blur-10 border border-[#C58DE7]/20 rounded-lg justify-center w-[120px] font-bold text-[#C58DE7] text-4xl h-[56px]"
+                  className="flex items-center bg-zinc-800/40 backdrop-blur-10 rounded-lg justify-center w-full font-bold max-w-[156px] max-sm:max-w-[113px] text-[#C58DE7] text-4xl h-[56px]"
                 >
                   {result}
                 </p>
               ))}
             </div>
           </div>
-        </div>
       </div>
     </div>
   );
